a97c8918945260f06d6724b16d4c5123d7d18685
Jacquilyn F <jacquilyn.fletcher89@gmail.com>
2025-02-05 23:49:05 -0600

    edit to htmlRoutes.ts

--- Develop/server/src/routes/htmlRoutes.ts
+++ Develop/server/src/routes/htmlRoutes.ts
@@ -1,14 +1,15 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { Router } from 'express';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const router = Router();
 
-// TODO: Define route to serve index.html
+// Define route to serve index.html
 router.get('/', (_req, res) => {
-    const filePath = path.join(__dirname, '..', 'dist', 'index.html'); // Adjust the path based on your project structure
-    res.sendFile(filePath);
-  });
-  
+  const filePath = path.join(__dirname, '..', '..', '..', 'client', 'dist', 'index.html'); 
+  res.sendFile(filePath);
+});
+
 export default router;
